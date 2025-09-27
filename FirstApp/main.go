9250004339b0bd392ce02fb2e674647f4d75c009
İlhan Enes Daniş <danisenes3@gmail.com>
2025-09-27 12:13:34 +0300

    Refactor Hello function and update tests for multilingual support

--- FirstApp/main.go
+++ FirstApp/main.go
@@ -4,8 +4,28 @@ func main() {
 
 }
 
-const englishHelloPrefix = "Hello, "
+const englishHelloPrefix = "Hello "
+const turkish = "Turkish"
+const turkishPrefix = "Merhaba "
+const french = "French"
+const frechPrefix = "Bonjour"
 
-func Hello(key string) string {
-	return englishHelloPrefix + key
+func Hello(key string, language string) string {
+	if key == "" {
+		key = "World"
+	}
+
+	return greetingprefix(language) + key
+}
+
+func greetingprefix(language string) (prefix string) {
+	switch language {
+	case french:
+		prefix = frechPrefix
+	case turkish:
+		prefix = turkishPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
 }
